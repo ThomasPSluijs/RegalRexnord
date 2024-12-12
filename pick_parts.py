62c4636e2be0ff163afa4290e88a21df30191934
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2024-12-12 13:51:30 +0100

    modified:   pick_parts.py

--- pick_parts.py
+++ pick_parts.py
@@ -13,7 +13,10 @@ class Pick_parts():
     def __init__(self, robot):
         self.belt_z = [0,0,-0.089,0,0,0]                                            #belt z location (should be )
         self.rotate_x = [0,0,0,math.radians(-15),math.radians(0),math.radians(0)]   #rotation about x axis of tool  
-        self.move_x = [-197/1000,0,0,0,0,0]                                         #total x movement when tool is rotated and aligned to pick up the parts
+        
+        self.part_pos_x_offset = 0.02                                #x offset so gripper starts before parts and does not crash down when going down
+        part_length = 0.187
+        self.move_x = [-part_length-self.part_pos_x_offset,0,0,0,0,0]         #total x movement when tool is rotated and aligned to pick up the parts. moves partlength + offset
 
         self.robot = robot  #robot class
 
@@ -30,15 +33,29 @@ class Pick_parts():
     #9: rotate parts into gripper
     #done
     def pick_parts(self, part_x, part_y):
+        #fast and slow speeds and accelerations. fast for general movements, slow for special movements. 
+        speed_fast = 0.5
+        acc_fast = 1
+
+        speed_slow = 0.1   
+        acc_slow = 0.5
+
         pickup_tcp = [-47.5/1000,-140/1000,135/1000,math.radians(0),math.radians(0),math.radians(0)]  #edge of part (x=centerpart, y=edge)
         self.robot.set_tool_frame(pickup_tcp)
 
+
+        start_rotation = [2.222, 2.248, 0.004]
+
+
         #step 2
-        #move to part x and part y, apply a offset on the x so the gripper is a bit before the part
+        #move to part x and part y, apply a offset on the x so the gripper is a bit before the part. also rotate to start rotation(level and aligned)
         cur_pos = self.robot.get_tcp_pos()
-        cur_pos[0] = part_x + 20/1000
+        cur_pos[0] = part_x + self.part_pos_x_offset
         cur_pos[1] = part_y
-        self.robot.moveL(cur_pos, 0.1, 0.1)
+        cur_pos[3] = start_rotation[0]
+        cur_pos[4] = start_rotation[1]
+        cur_pos[5] = start_rotation[2]
+        self.robot.moveL(cur_pos, speed_fast, acc_fast)
 
 
         #step 3
@@ -47,7 +64,7 @@ class Pick_parts():
         pose1 = self.robot.get_tcp_pos()
         pose2 = self.rotate_x
         result_pose = self.robot.pose_trans(pose1, pose2)
-        self.robot.move_l(result_pose, 0.5, 0.5)
+        self.robot.move_l(result_pose, speed_fast, acc_fast)
 
 
         #step 4
@@ -55,13 +72,13 @@ class Pick_parts():
         cur_pos = self.robot.get_tcp_pos()  
         cur_pos[2] = self.belt_z[2]
         logging.info(f"move to z pos: {cur_pos}")
-        self.robot.move_l(cur_pos, 1, 3)
+        self.robot.move_l(cur_pos, speed_slow, acc_slow)
 
 
         #step 5
         #perform a relative x movement so parts get picked up
         logging.info(f"pickup parts with relative x movment: {self.move_x}")
-        self.robot.move_add_l(self.move_x, 0.2, 3)
+        self.robot.move_add_l(self.move_x, speed_slow, acc_slow)
 
 
         #step 6
@@ -71,11 +88,11 @@ class Pick_parts():
         pose2 = self.rotate_x
         self.rotate_x[3] *= -1
         result_pose = self.robot.pose_trans(pose1, pose2)
-        self.robot.move_l(result_pose, 0.5, 3)
+        self.robot.move_l(result_pose, speed_slow, acc_slow)
 
 
         #step 7
-        #move a bit so parts are located better
+        #move a bit up and away so parts and rotate a bit so parts are better in the gripper
         logging.info("move a bit")
         pose1 = self.robot.get_tcp_pos()
         pose2 = self.rotate_x
@@ -83,14 +100,14 @@ class Pick_parts():
         result_pose = self.robot.pose_trans(pose1, pose2)
         result_pose[2] += 10/1000
         result_pose[0] += 5/1000
-        self.robot.move_l(result_pose, 0.5, 3)
+        self.robot.move_l(result_pose, speed_slow, acc_slow)
 
 
         #step 8
         #move up relative.
         relative_move=[0,0,200/1000,0,0,0]
         logging.info(f"move up relative to current position: {relative_move}")
-        self.robot.move_add_l(relative_move)
+        self.robot.move_add_l(relative_move, speed_fast, acc_fast)
 
 
         #step 9
@@ -100,7 +117,7 @@ class Pick_parts():
         pose1 = self.robot.get_tcp_pos()
         pose2 = relative_move
         result_pose = self.robot.pose_trans(pose1, pose2)
-        self.robot.move_l(result_pose, 0.1, 0.1)
+        self.robot.move_l(result_pose, speed_fast, acc_fast)
 
 
         #done
@@ -110,7 +127,6 @@ class Pick_parts():
 code below is to test this file. should be commented.
 '''
 
-'''
 #connect to robot
 robot = URControl("192.168.0.1")
 robot.connect()
@@ -122,4 +138,4 @@ robot.set_tool_frame(tool_frame=tool_frame)
 pick_part = Pick_parts(robot=robot)
 pick_part.pick_parts(0.1,0.2)
 
-robot.stop_robot_control() '''
+robot.stop_robot_control() 
