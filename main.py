f7f4b8d8e2222437ac54d23e17cddb9025d694e3
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2024-12-16 14:31:00 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import logging
 from UR5E_control import URControl
-import math
 from camera_position import CameraPosition         #used for scanning the belt for detected parts
 from pick_parts import *                           #used for picking parts from belt. needs x and y coordinates
 from place_parts import *                          #used for getting place locations and placing parts in boxes
 
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
@@ -48,11 +47,13 @@ def main_loop():
 
 logging.info("START")
 
+'''SETUP ROBOT'''
 # setup robot
 robot = URControl("192.168.0.1")  # defines robot.
 robot.connect()
 
 
+'''SETUP PLACE BOX, PACK BOX AND PART DEFINITIONS'''
 base_z = -12/1000
 #neeeds: total boxes, box pos (x and y center, z bottom), box dimensions: (x, y, z)
 box = Box(total_boxes=2, box_pos=[(-230/1000, -575/1000, base_z), [237/1000,-588/1000, base_z]], box_size=(0.365, 0.365, 0.180 ))
@@ -60,14 +61,18 @@ box = Box(total_boxes=2, box_pos=[(-230/1000, -575/1000, base_z), [237/1000,-588
 #needs: part width, part length, part height
 part = Part((0.187, 0.170, 0.009))
 
-# initialize vision camera class
-camera = CameraPosition(robot=robot)
-
 # initializes pack box class
 pack_box = Pack_Box(box=box, part=part, robot=robot)
 
 # initializes pick parts class
 pick_part = Pick_parts(robot=robot)
+
+
+''' SETUP VISION SYSTEM'''
+# initialize vision camera class
+camera = CameraPosition(robot=robot)
+
+
 
 # start main loop
 if __name__ == '__main__':
