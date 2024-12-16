24980fb5cf991b81e8186924ebeeeb8a73dc3d7d
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2024-12-16 11:13:18 +0100

    modified:   vision.py

--- vision.py
+++ vision.py
@@ -30,7 +30,7 @@ class ObjectDetector:
         self.labels = self.model.names
 
     def detect_objects(self, frame):
-        results = self.model(frame)
+        results = self.model.predict(source=frame, show=False)
         return results
     
     def transform_coordinates(self, xp, yp):
@@ -40,32 +40,36 @@ class ObjectDetector:
         yd = d * xp + e * yp + f
         return xd, yd
 
+    '''
     def process_frame(self, frame):
         logging.debug("Processing frame...")
         results = self.detect_objects(frame)
+
+        cv2.imshow("Detected Object", frame)
         
         for result in results:
             if result.boxes is not None:
                 for box in result.boxes:
-                    logging.debug(f"Detected object with bbox: {box.xyxy}")
-                    bbox = box.xyxy[0].cpu().numpy()
-                    bbox = [int(coord) for coord in bbox[:4]]
-                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
+                    if box.conf >= 0.5:
+                        logging.debug(f"Detected object with bbox: {box.xyxy}")
+                        bbox = box.xyxy[0].cpu().numpy()
+                        bbox = [int(coord) for coord in bbox[:4]]
+                        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (255, 0, 0), 2)
 
-                    x_left = bbox[0]
-                    y_middle = int((bbox[1] + bbox[3]) / 2)
-                    xd, yd = self.transform_coordinates(x_left, y_middle)
-                    target_position = [xd, yd, 0.5297075288092719, -2.1926151354478987, -2.2424509339020156, 0.009950454521939689]
+                        x_left = bbox[0]
+                        y_middle = int((bbox[1] + bbox[3]) / 2)
+                        xd, yd = self.transform_coordinates(x_left, y_middle)
+                        target_position = [xd, yd, 0.5297075288092719, -2.1926151354478987, -2.2424509339020156, 0.009950454521939689]
 
-                    # Print the detected camera coordinates and the resulting TCP position
-                    print(f"Detected (x, y): ({x_left}, {y_middle}) -> Calculated TCP Position: {target_position}")
+                        # Print the detected camera coordinates and the resulting TCP position
+                        print(f"Detected (x, y): ({x_left}, {y_middle}) -> Calculated TCP Position: {target_position}")
 
-                    # Draw a red dot at the left-most middle part and print coordinates
-                    cv2.circle(frame, (x_left, y_middle), 5, (0, 0, 255), -1)
-                    text = f'X: {x_left}, Y: {y_middle}'
-                    cv2.putText(frame, text, (x_left, y_middle - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        return frame
+                        # Draw a red dot at the left-most middle part and print coordinates
+                        cv2.circle(frame, (x_left, y_middle), 5, (0, 0, 255), -1)
+                        text = f'X: {x_left}, Y: {y_middle}'
+                        cv2.putText(frame, text, (x_left, y_middle - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                        cv2.imshow("Detected Object", frame)
+        return frame '''
     
 
 #Test
