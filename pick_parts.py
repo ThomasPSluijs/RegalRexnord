50a5e3f5d2ea80d6af8703d13e7c79d2c159096e
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2024-12-16 14:42:54 +0100

    modified:   pick_parts.py

--- pick_parts.py
+++ pick_parts.py
@@ -30,7 +30,7 @@ class Pick_parts():
         rotate_x = [0,0,0,math.radians(-23),math.radians(0),math.radians(0)]   #rotation about x axis of tool  
         
         part_pos_x_offset = 0.03                                #x offset so gripper starts before parts and does not crash down when going down
-        part_pos_x_offset_2 = 0.012                                #x offset so gripper does not go into wall
+        part_pos_x_offset_2 = 0.017                                #x offset so gripper does not go into wall
         part_length = 0.185
 
         #total x movement when tool is rotated and aligned to pick up the parts. moves partlength + offset
