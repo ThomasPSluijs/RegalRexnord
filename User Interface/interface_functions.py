0095c92c8f9ab9b53ceceb8cbd1d7f130cc3d57d
Daan Boekestijn <daanboek03@gmail.com>
2024-12-17 16:57:28 +0100

    made functions file

---
+++ User Interface/interface_functions.py
@@ -0,0 +1,11 @@
+def packing_mode_func():
+        #go to packing place
+        print('moving to packing position')
+
+def running_mode_func():
+        #go to start position
+        print('moving to start position')
+
+def start():
+        #start packing
+        print('packing')
