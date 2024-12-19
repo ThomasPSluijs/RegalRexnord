cdfab5365f30d30f3fdc90afc4d79e87c61b8334
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2024-12-19 12:00:24 +0100

    moved folder

---
+++ interface_main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import interface
+import time
+
+total_placements = 112
+placements = 100
+root = tk.Tk()
+ui = interface.UserInterface(root)
+ui.update_placements(placements)
+
+ui.update_progressbar(placements, total_placements)
+ui.update_activity("here you can type activitys")
+ui.update_live_feed()
+
+root.mainloop()
