ae4491f5ffca654d63d79b0fa4001015ace77eb2
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2025-01-08 11:10:58 +0100

    modified:   main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import interface
 total_placements = 112
 placements = 100    
 root = tk.Tk()
-ui = interface.UserInterface(root)
+ui = interface.UserInterface(root)   
      
 #ui.update_pr ogressbar(placements, total_placements)  
 #ui.update_activity("here you can type activitys")
