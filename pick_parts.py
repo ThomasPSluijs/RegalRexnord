0dd95a2cd567ec367cd6eec61b429a3c6e06e920
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2025-01-09 10:53:40 +0100

    tuned

--- pick_parts.py
+++ pick_parts.py
@@ -39,8 +39,8 @@ class Pick_parts():
 
 
         #part_x offset, one side needs a little bit more than the other side (or tune the camera calibration)
-        if part_y > 0.05: part_x += 16/1000 
-        else: part_x += 20/1000
+        if part_y > 0.05: part_x += 15/1000 
+        else: part_x += 19/1000
 
 
 
