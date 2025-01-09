136eeea47f61b43323af1cbb2a735f4960b29f0f
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2025-01-09 09:18:33 +0100

    run

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ total_placements = 112
 placements = 100    
 root = tk.Tk()
 ui = interface.UserInterface(root)   
-     
-#ui.update_pr ogressbar(placements, total_placements)  
-#ui.update_activity("here you can type activitys")
-                 
+                     
 root.mainloop()                 
                                                                                            
