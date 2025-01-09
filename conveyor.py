082254d2671511f71b9736ca2bf0b6484ea9f8e2
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2025-01-09 11:25:37 +0100

    modified:   conveyor.py

--- conveyor.py
+++ conveyor.py
@@ -8,6 +8,31 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
+
+
+class Conveyor():
+    def __init__(self):
+        self.running = False
+
+    def run(self, robot):
+        try:
+            output_id = 0
+
+            while True:
+                logging.info("Turning conveyor ON")
+                self.running = True
+                robot.set_digital_output(output_id, True)
+                time.sleep(15)  # Conveyor ON for 15 seconds
+                
+                logging.info("Turning conveyor OFF")
+                self.running = False
+                robot.set_digital_output(output_id, False)
+                time.sleep(45)  # Conveyor OFF for 45 seconds
+
+        except Exception as e:
+            logging.error(f"Error controlling conveyor: {e}")
+
+'''
 def main(robot):
     
     try:
@@ -26,4 +51,4 @@ def main(robot):
         logging.error(f"Error controlling conveyor: {e}")
 
 if __name__ == "__main__":
-    main()
+    main()'''
