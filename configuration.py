b57d1a417ea064f61040442d0616e16aaceafde7
SjoerdRosier <54005560+SjoerdRosier@users.noreply.github.com>
2025-01-20 15:18:15 +0100

    modified:   configuration.py

--- configuration.py
+++ configuration.py
@@ -4,5 +4,5 @@ pickup_tcp = [-47.5/1000,-147/1000,135/1000,0,0,0]          #edge of part (x=cen
 rotate_tcp = [-47.5/1000, 42/1000, 135/1000, 0, 0, 0]
 placement_tcp = [-47.5/1000,-49/1000,135/1000,0,0,0]        #center of part (x=center,y=center)
 
-speed_fast = 3
-acc_fast = 3
+speed_fast = 2.5
+acc_fast = 2.5
